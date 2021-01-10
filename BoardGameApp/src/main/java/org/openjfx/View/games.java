5ef3b7007dcb4593c500c7b1ecc39b2ce779a1a3
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-01-10 17:46:46 +0100

    ordinamento e filtri

--- BoardGameApp/src/main/java/org/openjfx/View/games.java
+++ BoardGameApp/src/main/java/org/openjfx/View/games.java
@@ -196,15 +196,16 @@ public class games {
 
         List<InfoGame> filteredGames = Lists.newArrayList();
         List<InfoGame> sortedList = Lists.newArrayList();
+        List<InfoGame> filteringResult = Lists.newArrayList();
 
-        if(categoria.getSelectionModel().getSelectedItem() != null) {
+        if(categoria.isVisible() && categoria.getSelectionModel().getSelectedItem() != null) {
             // filtraggio per categoria, crea una lista con tutti i giochi appartenenti alla categoria data
             int index1 = categoria.getSelectionModel().getSelectedIndex();
             filtraPerCategoria = categorie.get(index1);
             filteredGames = controller.filterByCategory(filtraPerCategoria);
         }
 
-        if(!data.getText().equals("")) {
+        if(data.isVisible() && !data.getText().equals("")) {
             if (Integer.parseInt(data.getText()) > 1900 || Integer.parseInt(data.getText()) < 2022) {
                 // filtraggio per anno
                 filtraPerAnno = Integer.parseInt(data.getText());
@@ -212,50 +213,108 @@ public class games {
             }
         }
 
-        if (players.getValue() > 0 || players.getValue() <= 16) {
+        if (players.isVisible() && (players.getValue() > 0 || players.getValue() <= 16) ){
             // filtraggio per numero di giocatori
             filtraGiocatori = (int) players.getValue();
             filteredGames = controller.filterByPlayers(filtraGiocatori);
         }
 
-        List<InfoGame> filteringResult = new ArrayList<InfoGame>(new HashSet<InfoGame>(sortedList));
-        showFilteringResult(filteringResult);
+        filteringResult = new ArrayList<InfoGame>(new HashSet<InfoGame>(filteredGames));
         // in filteredGames a questo punto c'e la lista dei giochi filtrati, se voglio ordinarli entro nell'if
 
-        /*if(filteredGames.size() == 0 && order.getSelectionModel().getSelectedItem() != null) {
-
+        if(filteredGames.size() == 0 && order.getSelectionModel().getSelectedItem() != null) {
+            // qui ci entro solo se non ho selezionato nessun filtro e quindi applico l'ordinamento a tutti i giochi
             int index1 = order.getSelectionModel().getSelectedIndex();
             String ordering = ordinamenti.get(index1);
 
             System.out.println("Ordinamento " + ordering);
 
             if(!ordering.equals("None")){
-                if (ordering.equals("Average ratings")) {
-                    logger.info("Ordino per rating");
-                    sortedList = controller.orderByAvgRating();
-                } else if (ordering.equals("Number of reviews")) {
-                    logger.info("Ordino per reviews");
-                    sortedList = controller.orderByNumReviews();
-                } else if (ordering.equals("Number of ratings")) {
-                    logger.info("Ordino per votes");
-                    sortedList = controller.orderByNumVotes();
-                } else {
-                    logger.info("Non ordino");
+                switch (ordering) {
+                    case "Average ratings" -> {
+                        logger.info("Ordino per rating");
+                        sortedList = controller.orderByAvgRating();
+                    }
+                    case "Number of reviews" -> {
+                        logger.info("Ordino per reviews");
+                        sortedList = controller.orderByNumReviews();
+                    }
+                    case "Number of ratings" -> {
+                        logger.info("Ordino per votes");
+                        sortedList = controller.orderByNumVotes();
+                    }
+                    default -> logger.info("Non ordino");
                 }
 
-                List<InfoGame> filteringResult = new ArrayList<InfoGame>(new HashSet<InfoGame>(sortedList));
+                 filteringResult = new ArrayList<InfoGame>(new HashSet<InfoGame>(sortedList));
                 showFilteringResult(filteringResult);
             }
-        } */
+        }
+
+        if(filteredGames.size() != 0 && order.getSelectionModel().getSelectedItem() != null){
+            // ho applicato dei filtri e voglio ordinare le cose filtrate
+            int index1 = order.getSelectionModel().getSelectedIndex();
+            String ordering = ordinamenti.get(index1);
+
+            System.out.println("Ordinamento con filtri" + ordering);
+
+            filteringResult = orderingResult(filteredGames,ordering);
+        }
+
+        filteringResult = new ArrayList<InfoGame>(new HashSet<InfoGame>(filteredGames));
+        showFilteringResult(filteringResult);
     }
 
+    private List<InfoGame> orderingResult(List<InfoGame> target, String mode) {
+
+        if(!mode.equals("None")){
+            switch (mode) {
+                case "Average ratings" -> {
+                    logger.info("Ordino per rating");
+                    target.sort(new averageRatingComparator());
+                }
+                case "Number of reviews" -> {
+                    logger.info("Ordino per reviews");
+                    target.sort(new numberOfReviewsComparator());
+                }
+                case "Number of ratings" -> {
+                    logger.info("Ordino per rating");
+                    target.sort(new numberOfRatingsComparator());
+                }
+                default -> logger.info("Non ordino");
+            }
+        }
+
+        System.out.println(target);
+        return target;
+    }
+
+    static class averageRatingComparator implements Comparator<InfoGame> {
+        @Override
+        public int compare(InfoGame a, InfoGame b) {
+            return Double.compare(a.getAvgRating(), b.getAvgRating());
+        }
+    }
+
+    static class numberOfReviewsComparator implements Comparator<InfoGame> {
+        @Override
+        public int compare(InfoGame a, InfoGame b) {
+            return Integer.compare(a.getNumReviews(), b.getNumReviews());
+        }
+    }
+
+    static class numberOfRatingsComparator implements Comparator<InfoGame> {
+        @Override
+        public int compare(InfoGame a, InfoGame b) {
+            return Integer.compare(a.getNumVotes(), b.getNumVotes());
+        }
+    }
 
     private void showFilteringResult(List<InfoGame> filteringResult) {
         logger.info("show filtering result");
         if (filteringResult != null) {
             System.out.println("Lunghezza lista " + filteringResult.size());
             for (int i = 0; i < filteringResult.size() && i < 6; i++) {
-                logger.info("mostro");
                 InfoGame g = (InfoGame) filteringResult.get(i);
                 TitledPane gioco = (TitledPane) App.getScene().lookup("#fullgame" + (i + 1));
                 Text category1 = (Text) App.getScene().lookup("#category1" + (i + 1));
@@ -293,7 +352,6 @@ public class games {
 
     @FXML
     void caricaCategorie() throws IOException {
-        logger.info("Carico le categorie");
         Scene scene = App.getScene(); // recupero la scena della signup
         ComboBox cat1 = (ComboBox) scene.lookup("#category");
 
