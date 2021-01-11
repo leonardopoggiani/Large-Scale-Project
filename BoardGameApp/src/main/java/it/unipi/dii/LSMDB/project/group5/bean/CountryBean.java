7979be8c004c2402e0afde5f95e69ee9103e890c
gaiaanastasi <gaia.anastasi98@gmail.com>
2021-01-11 20:09:05 +0100

    Ripristino Gaia

---
+++ BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/bean/CountryBean.java
@@ -0,0 +1,22 @@
+package it.unipi.dii.LSMDB.project.group5.bean;
+
+public class CountryBean {
+    String country;
+    private int numUser;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public int getNumUser() {
+        return numUser;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setNumUser(int numUser) {
+        this.numUser = numUser;
+    }
+}
--- BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/persistence/MongoDBManager/UserDBManager.java
+++ BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/persistence/MongoDBManager/UserDBManager.java
@@ -5,14 +5,16 @@ import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import it.unipi.dii.LSMDB.project.group5.bean.UserBean;
 
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static com.mongodb.client.model.Filters.eq;
+import static it.unipi.dii.LSMDB.project.group5.persistence.MongoDBManager.ArticleDBManager.*;
 
 
-public class SignupLoginDBManager extends MongoDBManager {
+public class UserDBManager extends MongoDBManager {
     public static void signup(UserBean u){
         MongoCollection<Document> collection = getCollection("Users");
         Document doc = new Document("username", u.getUsername()).append("name", u.getName()).append("surname", u.getSurname())
@@ -33,5 +35,19 @@ public class SignupLoginDBManager extends MongoDBManager {
         update.append("$set", setLastLogin);
         collection.updateOne(eq("username", username), update);
     }
+	
+	 protected static UserBean fillUserFields (Document next){
+        UserBean u = new UserBean();
+        u.setUsername((next.get("username") == null)? "":next.get("username").toString());
+        u.setName((next.get("name")==null)?"": next.get("name").toString());
+        u.setSurname((next.get("surname")==null)?"":next.get("surname").toString());
+        u.setAge((next.get("age")==null) ? 18: Integer.parseInt(next.get("age").toString()));
+        u.setRegistered((next.get("registered") == null) ? 2020 : Integer.parseInt(next.get("registered").toString()) );
+        u.setLastLogin((next.get("last_login")==null)? new Timestamp(System.currentTimeMillis()) : convertStringToTimestamp(next.get("last_login").toString()));
+        u.setCountry(next.get("country")==null? "Non specificato" : next.get("country").toString());
+        u.setUpdated((next.get("updated")==null)? new Timestamp(System.currentTimeMillis()) : convertStringToTimestamp(next.get("updated").toString()));;
+        return u;
+    }
+
 
 }
