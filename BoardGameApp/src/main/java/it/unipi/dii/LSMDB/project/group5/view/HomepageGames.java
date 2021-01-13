93c34c7cb46ad3f2c7d2f1a5f50fa2b97c2772db
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-01-13 12:32:17 +0100

    article page refactor

--- BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/view/HomepageGames.java
+++ BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/view/HomepageGames.java
@@ -8,8 +8,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
