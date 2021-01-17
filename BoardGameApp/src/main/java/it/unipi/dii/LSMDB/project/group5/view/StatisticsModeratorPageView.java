f2e37f0b505bbf6c4305f54ae70c261327d75221
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-01-17 19:02:31 +0100

    moderator page

--- BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/view/StatisticsModeratorPageView.java
+++ BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/view/StatisticsModeratorPageView.java
@@ -1,6 +1,7 @@
 package it.unipi.dii.LSMDB.project.group5.view;
 
 import it.unipi.dii.LSMDB.project.group5.bean.InfluencerInfoBean;
+import it.unipi.dii.LSMDB.project.group5.bean.VersatileUser;
 import it.unipi.dii.LSMDB.project.group5.controller.AnalyticsDBController;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
@@ -21,12 +22,42 @@ public class StatisticsModeratorPageView {
     Text influencer3;
 
     @FXML
+    Text dislike1;
+
+    @FXML
+    Text dislike2;
+
+    @FXML
+    Text dislike3;
+
+    @FXML
+    Text versatile1;
+
+    @FXML
+    Text versatile2;
+
+    @FXML
+    Text versatile3;
+
+    @FXML
     void initialize() {
         List<InfluencerInfoBean> list = controller.getNumLikeForEachInfluencer();
 
         influencer1.setText(list.get(0).getInfluencer() + ": " + list.get(0).getCount());
         influencer2.setText(list.get(1).getInfluencer() + ": " + list.get(1).getCount());
         influencer3.setText(list.get(2).getInfluencer() + ": " + list.get(2).getCount());
+
+        list = controller.getNumDisLikeForEachInfluencer();
+
+        dislike1.setText(list.get(0).getInfluencer() + ": " + list.get(0).getCount());
+        dislike2.setText(list.get(1).getInfluencer() + ": " + list.get(1).getCount());
+        dislike3.setText(list.get(2).getInfluencer() + ": " + list.get(2).getCount());
+
+        List<VersatileUser> listUser = controller.mostVersatileUsers("normalUser");
+
+        versatile1.setText(listUser.get(0).getUsername() + ": " + listUser.get(0).getHowManyCategories());
+        versatile2.setText(listUser.get(1).getUsername() + ": " + listUser.get(1).getHowManyCategories());
+        versatile3.setText(listUser.get(2).getUsername() + ": " + listUser.get(2).getHowManyCategories());
 
     }
     
