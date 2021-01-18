5df7c51d6fa2c087120ed316d81b36d061565eee
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-01-18 19:23:09 +0100

    Update AnalyticsDBManager.java

--- BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/persistence/MongoDBManager/AnalyticsDBManager.java
+++ BoardGameApp/src/main/java/it/unipi/dii/LSMDB/project/group5/persistence/MongoDBManager/AnalyticsDBManager.java
@@ -285,7 +285,7 @@ public class AnalyticsDBManager {
         try(MongoCursor<Document> cursor = collection.aggregate(Arrays.asList(projection1, match, match1, group, group2, projection,sort)).iterator()) {
 
             while (cursor.hasNext()) {
-                System.out.println(cursor.next().toJson());
+                // System.out.println(cursor.next().toJson());
                 Document next = cursor.next();
                 ActivityBean a = new ActivityBean();
                 a.setCountry((next.get("country") == null) ? "" : (next.get("country").toString()));
